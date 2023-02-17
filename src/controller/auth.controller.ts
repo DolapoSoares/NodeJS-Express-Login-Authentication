dfe4ecf6f6dd31c2f4b49f3b01e13e61a6d200f8
Dolapo Soares <dsoares@econnectnp.com>
2023-02-17 14:32:55 +0100

    created the authenticated user endpoint

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -16,4 +16,90 @@ export const Register = async (req: Request, res: Response) => {
         email,
         password: await bcryptjs.hash(password, 12)
     })
+
+    res.send(user)
+}
+
+export const Login = async (req: Request, res : Response) => {
+
+    const { email , password } = req.body;
+
+    const user = await repository.findOne({
+        where: {
+            email: email
+        }
+    });
+
+    if (!user) {
+        return res.status(400).send({
+            message: "Incorrect Information"
+        })
+    }
+
+    if (!await bcryptjs.compare(password, user.password)) {
+
+        return res.status(400).send({
+            message: "Invalid Information"
+        })
+    }
+
+    const accessToken = sign({
+        id: user.id
+    }, "access_secret", {expiresIn : 60 * 60});
+
+    const refreshToken =sign({
+        id: user.id
+    }, "refresh_token", {expiresIn: 24 * 60 
+    * 60})
+
+    res.cookie('accessToken', accessToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000
+    });
+
+    res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    });
+
+    res.send({
+        message: "Successful"
+    });
+}
+
+export const AuthenticatedUser = async (req: Request, res: Response) => {
+    try {
+        console.log(req.cookies);
+        const accessToken = req.cookies['accessToken'];
+
+        const payload: any = verify(accessToken, "access_secret");
+
+        if(!payload) {
+            return res.status(401).send({
+                message: 'Unauthenticated'
+            })
+        }
+
+        const user = await repository.findOne({
+            where: {
+                id: payload.id
+            }
+        });
+
+        if (!user) {
+            return res.status(401).send({
+                message: 'Unauthenticated'
+            })
+        }
+
+        const {password, ...data} = user;
+
+        res.send(data);
+
+    }catch(e) {
+        console.log(e)
+        return res.status(401).send({
+            message: 'Unauthenticated'
+        })
+    }
 }
